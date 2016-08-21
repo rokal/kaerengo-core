a0d06ab7da73a78cd103acead6822e78ba57b608
Roland <rskalmogo@gmail.com>
2016-08-21 15:28:52 -0400

    refactoring on project  base structure

---
+++ src/main/java/entities/Aptitude.java
@@ -0,0 +1,6 @@
+package entities;
+
+public enum Aptitude {
+	RIGOUREUX, TRAVAILLEUR, LOGIQUE, FAINEANT, AUTONOME
+
+}
---
+++ src/main/java/entities/Domaine.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+public enum Domaine {
+
+	DOMAINE_COMMERCE_COMMUNICATION, DOMAINE_GESTION_DROIT, DOMAINE_INFORMATIQUE_TELECOM, DOMAINE_INGENIERIE, DOMAINE_BTP_IMMOBILIER, DOMAINE_AGRO_BIO_CHIMIE,
+
+}
