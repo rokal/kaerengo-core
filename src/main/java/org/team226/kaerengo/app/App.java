2071d6ffc5fd5a1315efbbeb51f2f484930f7e06
Roland <rskalmogo@gmail.com>
2016-08-21 18:20:08 -0400

    GSON added

--- src/main/java/org/team226/kaerengo/app/App.java
+++ src/main/java/org/team226/kaerengo/app/App.java
@@ -1,9 +1,10 @@
 package org.team226.kaerengo.app;
 
+import static spark.Spark.*;
+
 public class App {
 
 	public static void main(String[] args) {
-
+		get("/hello", (req, res) -> "Hello World");
 	}
-
 }
